bc835b46a27b5c14428cb1c21379b116346171e7
r4phael <albus.zly@gmail.com>
2020-08-18 17:18:42 +0800

    修复历史交易数量错误

--- src/crawl/history_price_crawler.py
+++ src/crawl/history_price_crawler.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.config.urls import *
 from src.util.requester import *
 from src.util.logger import log
@@ -13,11 +15,14 @@ def crawl_item_history_price(index, item, total_price_number):
 
     if steam_history_prices is not None:
         raw_price_history = steam_history_prices['prices']
-        days = min(len(raw_price_history), 7)
+        if len(raw_price_history) > 0:
+            days = min((datetime.today().date() - datetime.strptime(raw_price_history[0][0], '%b %d %Y %H: +0').date()).days, 7)
+        else:
+            days = 0
         for pair in reversed(raw_price_history):
             if len(pair) == 3:
                 history_prices.append(float(pair[1]))
-            if len(history_prices) == days:
+            if (datetime.today().date() - datetime.strptime(pair[0], '%b %d %Y %H: +0').date()).days > days:
                 break
 
         # set history price if exist
