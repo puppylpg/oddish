58e07e8b8c6cdbc6a5a5ca34dc38c2b997259c4f
ccinv <avaicode@gmail.com>
2020-10-23 16:04:08 +0800

    fix empty cache exceptions #31

--- src/util/cache.py
+++ src/util/cache.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 import hashlib
 from src.config.definitions import CACHE_DIR, FORCE_CRAWL, URL_CACHE_HOUR
 from src.util.logger import log
@@ -7,6 +8,13 @@ from src.util.logger import log
 cache_root = os.path.join(os.getcwd(), CACHE_DIR)
 if not os.path.exists(cache_root):
     os.mkdir(cache_root)
+
+def is_json(js):
+    try:
+        json_object = json.loads(js)
+    except ValueError as e:
+        return False
+    return True
 
 def url_id(url):
     return hashlib.sha1(url.encode("utf-8")).hexdigest()
@@ -18,6 +26,9 @@ def exist(url):
     urlid = url_id(url)
     if not os.path.exists(os.path.join(cache_root,urlid)):
         return False
+    with open(os.path.join(cache_root, urlid), "r", encoding='utf-8') as f:
+        if not is_json(f.read()):
+            return False
     mtime = os.path.getmtime(os.path.join(cache_root,urlid))
     return (time.time() - mtime) / 3600 <= URL_CACHE_HOUR
 
@@ -28,6 +39,8 @@ def fetch(url):
         return f.read()
 
 def store(url, data):
+    if exist(url):
+        return
     urlid = url_id(url)
     f = open(os.path.join(cache_root,urlid), "w", encoding='utf-8')
     f.write(data)
