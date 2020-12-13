2f4a475be4ca0490ce5e71ef80c9d17a1c8bda3a
zwwskm <954396495@qq.com>
2020-12-13 22:10:49 +0800

    solve unbound variable

--- src/util/timer.py
+++ src/util/timer.py
@@ -9,9 +9,9 @@ from src.util.logger import log
 def sleep_awhile(mode = 0):
     low = max(FREQUENCY_INTERVAL_LOW, 2)
     high = max(2, FREQUENCY_INTERVAL_HIGH)
-    if mode == 0:
-        interval = random.randint(low, high)
-    elif mode == 1:
+    if mode == 1:
         interval = 1/(random.randint(5, 10))
+    else:
+        interval = random.randint(low, high)
     log.info("sleep {}s at {}".format(interval, datetime.datetime.now()))
     time.sleep(interval)
